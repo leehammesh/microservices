9bb956dfac2084f57c6e0be687fd30d937c0cf7c
leehammesh <126901848+leehammesh@users.noreply.github.com>
2023-09-03 23:14:54 +0530

    Update index.js

--- cart-microservice-nodejs/src/routes/index.js
+++ cart-microservice-nodejs/src/routes/index.js
@@ -6,3 +6,18 @@ const CartRoute = require('./cart/cart');
 router.use(CartRoute)
 
 module.exports = router
+const express = require('express')
+const router = express.Router();
+
+const CartRoute = require('./cart/cart');
+
+router.use(CartRoute)
+
+module.exports = routerconst express = require('express')
+const router = express.Router();
+
+const CartRoute = require('./cart/cart');
+
+router.use(CartRoute)
+
+module.exports = router
