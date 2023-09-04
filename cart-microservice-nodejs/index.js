dd80e77c8de5a4d373d5236ae74a2d11cc1ac4a4
leehammesh <126901848+leehammesh@users.noreply.github.com>
2023-09-04 16:51:11 +0530

    Update index.js

--- cart-microservice-nodejs/index.js
+++ cart-microservice-nodejs/index.js
@@ -329,3 +329,336 @@ app.listen(PORT, () => {
     console.log(`Cart Microservice Started...`);
     console.log(`Node Server is running on port ${PORT}.`);
 });
+
+
+const express = require("express");
+
+
+const app = express();
+const route = require('./src/routes')
+
+var corsOptions = {
+    origin: "*"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+// simple route
+app.get("/welcome", (req, res) => {
+    res.json({ message: "Welcome to the application." });
+});
+
+
+
+
+// set port, listen for requests
+const PORT = process.env.PORT || 1004;
+app.listen(PORT, () => {
+    console.log(`Cart Microservice Started...`);
+    console.log(`Node Server is running on port ${PORT}.`);
+});
+const express = require("express");
+
+
+const app = express();
+const route = require('./src/routes')
+
+var corsOptions = {
+    origin: "*"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+// simple route
+app.get("/welcome", (req, res) => {
+    res.json({ message: "Welcome to the application." });
+});
+
+
+
+
+// set port, listen for requests
+const PORT = process.env.PORT || 1004;
+app.listen(PORT, () => {
+    console.log(`Cart Microservice Started...`);
+    console.log(`Node Server is running on port ${PORT}.`);
+});
+const express = require("express");
+
+
+const app = express();
+const route = require('./src/routes')
+
+var corsOptions = {
+    origin: "*"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+// simple route
+app.get("/welcome", (req, res) => {
+    res.json({ message: "Welcome to the application." });
+});
+
+
+
+
+// set port, listen for requests
+const PORT = process.env.PORT || 1004;
+app.listen(PORT, () => {
+    console.log(`Cart Microservice Started...`);
+    console.log(`Node Server is running on port ${PORT}.`);
+});
+const express = require("express");
+
+
+const app = express();
+const route = require('./src/routes')
+
+var corsOptions = {
+    origin: "*"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+// simple route
+app.get("/welcome", (req, res) => {
+    res.json({ message: "Welcome to the application." });
+});
+
+
+
+
+// set port, listen for requests
+const PORT = process.env.PORT || 1004;
+app.listen(PORT, () => {
+    console.log(`Cart Microservice Started...`);
+    console.log(`Node Server is running on port ${PORT}.`);
+});
+const express = require("express");
+
+
+const app = express();
+const route = require('./src/routes')
+
+var corsOptions = {
+    origin: "*"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+// simple route
+app.get("/welcome", (req, res) => {
+    res.json({ message: "Welcome to the application." });
+});
+
+
+
+
+// set port, listen for requests
+const PORT = process.env.PORT || 1004;
+app.listen(PORT, () => {
+    console.log(`Cart Microservice Started...`);
+    console.log(`Node Server is running on port ${PORT}.`);
+});
+
+const express = require("express");
+
+
+const app = express();
+const route = require('./src/routes')
+
+var corsOptions = {
+    origin: "*"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+// simple route
+app.get("/welcome", (req, res) => {
+    res.json({ message: "Welcome to the application." });
+});
+
+
+
+
+// set port, listen for requests
+const PORT = process.env.PORT || 1004;
+app.listen(PORT, () => {
+    console.log(`Cart Microservice Started...`);
+    console.log(`Node Server is running on port ${PORT}.`);
+});
+const express = require("express");
+
+
+const app = express();
+const route = require('./src/routes')
+
+var corsOptions = {
+    origin: "*"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+// simple route
+app.get("/welcome", (req, res) => {
+    res.json({ message: "Welcome to the application." });
+});
+
+
+
+
+// set port, listen for requests
+const PORT = process.env.PORT || 1004;
+app.listen(PORT, () => {
+    console.log(`Cart Microservice Started...`);
+    console.log(`Node Server is running on port ${PORT}.`);
+});
+const express = require("express");
+
+
+const app = express();
+const route = require('./src/routes')
+
+var corsOptions = {
+    origin: "*"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+// simple route
+app.get("/welcome", (req, res) => {
+    res.json({ message: "Welcome to the application." });
+});
+
+
+
+
+// set port, listen for requests
+const PORT = process.env.PORT || 1004;
+app.listen(PORT, () => {
+    console.log(`Cart Microservice Started...`);
+    console.log(`Node Server is running on port ${PORT}.`);
+});
+const express = require("express");
+
+
+const app = express();
+const route = require('./src/routes')
+
+var corsOptions = {
+    origin: "*"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+// simple route
+app.get("/welcome", (req, res) => {
+    res.json({ message: "Welcome to the application." });
+});
+
+
+
+
+// set port, listen for requests
+const PORT = process.env.PORT || 1004;
+app.listen(PORT, () => {
+    console.log(`Cart Microservice Started...`);
+    console.log(`Node Server is running on port ${PORT}.`);
+});
+const express = require("express");
+
+
+const app = express();
+const route = require('./src/routes')
+
+var corsOptions = {
+    origin: "*"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+// simple route
+app.get("/welcome", (req, res) => {
+    res.json({ message: "Welcome to the application." });
+});
+
+
+
+
+// set port, listen for requests
+const PORT = process.env.PORT || 1004;
+app.listen(PORT, () => {
+    console.log(`Cart Microservice Started...`);
+    console.log(`Node Server is running on port ${PORT}.`);
+});
